cb89d59150fa6b17df6982a0e51841512a12f21c
eb46 <ericjbondoc@gmail.com>
2020-07-19 14:03:36 -0700

    removed cors issues

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors({
-  origin:['https://deskly.herokuapp.com/'],
-  methods:['GET', 'POST', 'DELETE', 'PUT']
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
